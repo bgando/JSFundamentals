eba092df5a610ccb7b43c39c9e0b2b5651d51063
Bianca <gando.bia@gmail.com>
2014-04-16 18:37:18 -0700

    initial 3

---
+++ Day3Challenges.js
@@ -0,0 +1,28 @@
+//change func so that it works with the for loop
+//with semi colons
+=================================================
+var func = function(){};
+for(var i = 0; i < func.length; i++){
+  console.log(func[i]);
+}
+//change obj so that it works with the for loop
+//with semi colons
+=================================================
+var obj = {};
+for(var i = 0; i < func.length; i++){
+  console.log(obj[i]);
+}
+
+//write a function that changes an object into an array of it's values
+//input => {one: 1, two: 2, three: 3, four: 4} 
+//output => [1,2,3,4]
+
+//now write a function that takes an object and creates and array of it's 
+//properties
+//input => {one: 1, two: 2, three: 3, four: 4} 
+//output => ['one','two', 'three', 'four']
+
+//what is the difference between a side effect of a function and
+//what a function returns?
+
+//use a ternary operator
---
+++ FuncIntro.js
@@ -0,0 +1,52 @@
+//ternary operators
+=================================================
+var test = true;
+//if test is true, log true, else log false
+test ? console.log(true) : console.log(false);
+//logic operators
+=================================================
+&&
+
+||
+
+!!
+
+!
+
+//truthy + falsy
+=================================================
+JavaScript Falsy Values: 
+null, false, 0 undefined, NaN, and “” (this last item is an empty string).
+— Note that Infinity, which is a special number like NaN, is truthy; it is 
+not falsy, while NaN is falsy.
+JavaScript Truthy Values: Anything other than the falsy values.
+// functions + logic operators
+=================================================
+
+function documentTitle(theTitle)
+  theTitle  = theTitle || "Untitled Document";
+}
+
+function isAdult(age) {
+  if (age && age > 17) {
+  return true;
+}
+else {
+  return false;
+  }
+}
+Use this:
+
+function isAdult(age) {
+   return age && age > 17 ;
+}
+
+if (userName) {
+  logIn (userName);
+}
+ else {
+   signUp ();
+}
+Use this:
+
+userName && logIn (userName) || signUp ();
